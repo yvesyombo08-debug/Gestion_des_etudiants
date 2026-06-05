27c3743b9fa729c7a39df35961ff790beb74eac4
YL <yvesyombo.08@gmail.com>
2026-06-06 00:01:31 +0100

    Jour 3: Ajout stockage avec array list

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-public class main {
+import java.util.ArrayList;
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        ArrayList<Etudiant> etudiants = new ArrayList<>();   
         int choix;
         do {
             System.out.println("\n===== MENU =====");
